d3c43f09970882d463c6167d6cb3c18229da1759
Dylan Wiafe <dylanwiafe@gmail.com>
2021-05-28 16:20:32 -0400

    styles

--- client/src/Coin.js
+++ client/src/Coin.js
@@ -3,20 +3,20 @@ import "./Coin.scss";
 
 const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
   return (
-    <div className="coin">
-      <div className="coin__row">
-        <div className="coin__image-name-symbol">
-          <img src={image} alt="cryptocurrency logo" className="coin__image" />
-          <h1 className="coin__name">{name}</h1>
-          <p className="coin__symbol">{symbol}</p>
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="cryptocurrency logo" className="coin-image" />
+          <h1 className="coin-name">{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
         </div>
-        <div className="coin__data">
-          <p className="coin__price">${price}</p>
-          <p className="coin__volume">${volume.toLocaleString()}</p>
+        <div className="coin-data">
+          <p className="coin-price">${price}</p>
+          <p className="coin-volume">${volume.toLocaleString()}</p>
           {priceChange < 0 ? (
-            <p className="coin__percent-red">{priceChange.toFixed(2)}</p>
+            <p className="coin-percent-red">{priceChange.toFixed(2)}</p>
           ) : (
-            <p className="coin__percent-green">{priceChange.toFixed(2)}%</p>
+            <p className="coin-percent-green">{priceChange.toFixed(2)}%</p>
           )}
         </div>
       </div>
