7da466371e1cb2e2ca38ca70aeac5198e29b1380
Dylan Wiafe <dylanwiafe@gmail.com>
2021-05-27 22:26:14 -0400

    new folders & files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+// import Hero from "./components/Hero/Hero";
 function App() {
   return (
     <div className="App">
